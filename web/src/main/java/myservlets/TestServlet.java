cb041877ce81da8dec49b8bf372c704e563da13e
Vincent <Vincent@192.168.1.10>
2014-12-21 02:51:56 +0100

    TestServlet : test du nouveau UserDao.
    Le but premier était de tester les nouvelles fonctions de UserDao. Elles sont tous utilisées sauf le filtrage par mail.
    
    Le second était de commencer à voir comment on peut catcher les erreurs dans les requêtes (typiquement delete d'un truc qui existe pas, insert d'un truc qui respecte pas un Unique)... Et c'est un sacré bordel. Les Ejb balancent des exceptions dans tous les sens, et ce qu'on récupère dans les servlet est une pile de 5 ou 6 exceptions qu'il faut éplucher pour trouver ce qui a merdé. Je propose un exemple bricolé sur le insert (vérifie si c'est la contrainte d'unicité sur les mail qui a foiré).

--- web/src/main/java/myservlets/TestServlet.java
+++ web/src/main/java/myservlets/TestServlet.java
@@ -17,12 +17,52 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.annotation.Resource;
+import javax.ejb.EJBException;
+import javax.persistence.PersistenceException;
+import javax.transaction.SystemException;
+import javax.transaction.UserTransaction;
+import javax.validation.ConstraintViolationException;
 
 @WebServlet(name = "TestServlet", urlPatterns = {"/TestServlet"})
 public class TestServlet extends HttpServlet {
 
     @EJB
     private UserDao userDao;
+
+    public String htmlTableUser() {
+        String msg = "";
+        List<User> list = this.userDao.getAll();
+        msg += "<br/><br/><form action=\"/web/TestServlet\">\n";
+        msg += "  <input type=\"hidden\" name=\"method\" value=\"insert\"/>\n";
+        msg += "  mail : <input type=\"text\" name=\"mail\"/>\n";
+        msg += "  password : <input type=\"text\" name=\"password\"/>\n";
+        msg += "  <input type=\"submit\" value=\"Ajouter\"/>\n";
+        msg += "</form><br/><br/>";
+
+        msg += "<table><thead>\n";
+        msg += "  <th>Pk</th>\n";
+        msg += "  <th>mail</th>\n";
+        msg += "  <th>balance</th>\n";
+        msg += "  <th>isAdmin</th>\n";
+        msg += "</thead><tbody>\n";
+        for (User user : list) {
+            msg += "  <tr><form action=\"/web/TestServlet\">\n";
+            msg += "    <input type=\"hidden\" name=\"method\" value=\"update\">\n";
+            msg += "    <input type=\"hidden\" name=\"pk\" value=\"" + user.getIdUser() + "\">\n";
+            msg += "    <td>" + user.getIdUser() + "</td>\n";
+            msg += "    <td>" + user.getMail() + "</td>\n";
+            msg += "    <td><input type=\"number\" name=\"balance\" value=\"" + user.getBalance() + "\" min=\"0\"/></td>\n";
+            msg += "    <td>" + user.getIsAdmin() + "</td>\n";
+            msg += "    <td><input type=\"submit\" value=\"Modifier\"/></td>\n";
+            msg += "    <td><a href=\"/web/TestServlet?method=delete&amp;pk=" + user.getIdUser() + "\">Supprimer</a></td>\n";
+            msg += "  </form></tr>\n";
+        }
+        msg += "</tbody></table>\n";
+        return msg;
+    }
 
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
@@ -31,32 +71,61 @@ public class TestServlet extends HttpServlet {
         System.out.println("papa: " + para);
 
         if (para.equals("insert")) {
-            String userid = request.getParameter("userid");
-            String username = request.getParameter("username");
-            String usercode = request.getParameter("usercode");
+            String mail = request.getParameter("mail");
             String password = request.getParameter("password");
+
             User user = new User();
-            user.setUserid(Integer.parseInt(userid));
-            user.setUsercode(usercode);
-            user.setUsername(username);
+            user.setMail(mail);
             user.setPassword(password);
-            this.userDao.save(user);
-            msg = "insert successfully：" + username;
-        }else if (para.equals("delete")) {
-            String usercode = request.getParameter("usercode");
-            User user = this.userDao.getByUserCode(usercode);
-            if (user != null) {
-                this.userDao.delete(user);
+            try {
+                this.userDao.save(user);
+                msg = "insert successful : " + user.getIdUser();
+            } catch (EJBException e) {
+                // going to the interesting exception in the exception stack.
+                Throwable cause = e.getCause().getCause();
+                if (cause instanceof PersistenceException) {
+                    PersistenceException pe = (PersistenceException) cause;
+                    String message = pe.getMessage();
+                    // put some elseif to check which constraint crashed.
+                    if (message.contains("login_UNIQUE")) {
+                        msg = "Insert failed : mail already exists.";
+                    } else {
+                        msg = "Insert failed : unknown reason.";
+                    }
+                } else {
+                    msg = "Insert failed : unknown reason.";
+                }
             }
-            msg = "delete successfully：" + user.getUsername();
-        }else if (para.equals("findall")) {
-            System.out.println("test");
-            List<User> list = this.userDao.getAll();
-            for (User user : list) {
-                msg += "   " + user.getUsername();
+            msg += "\n <br/>\n" + htmlTableUser();
+
+        } else if (para.equals("delete")) {
+            int pk = Integer.parseInt(request.getParameter("pk"));
+            try {
+                User user = this.userDao.getByIdUser(pk);
+                userDao.delete(user);
+                msg = "delete successful : " + pk;
+            } catch (EJBException e) {
+                //msg = "delete failed : invalid pk.";
             }
-        }else{
-            msg = "method Unknown";
+            msg += htmlTableUser();
+        } else if (para.equals("findall")) {
+            msg = htmlTableUser();
+        } else if (para.equals("update")) {
+            int pk = Integer.parseInt(request.getParameter("pk"));
+            int balance = Integer.parseInt(request.getParameter("balance"));
+            
+            try {
+                User user = userDao.getByIdUser(pk);
+                user.setBalance(balance);
+                
+                userDao.update(user);
+                msg = "Update successful.";
+            } catch (EJBException e) {
+                msg = "Update failed.";
+            }
+            msg += htmlTableUser();
+        } else {
+            msg = "method Unknown <br/>\n" + htmlTableUser();
         }
 
         response.setContentType("text/html;charset=UTF-8");
