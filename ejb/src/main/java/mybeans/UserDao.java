24eb62768464f933f23d8c1430b8198164d326e3
Vincent <Vincent@192.168.1.10>
2014-12-21 02:45:39 +0100

    Mise à jour de UserDao.
    - actualisation pour être à jour avec le nouveau schéma de BD.
    - recherche par pk, ou par mail.

--- ejb/src/main/java/mybeans/UserDao.java
+++ ejb/src/main/java/mybeans/UserDao.java
@@ -1,49 +1,64 @@
 /*
-* To change this license header, choose License Headers in Project Properties.
-* To change this template file, choose Tools | Templates
-* and open the template in the editor.
-*/
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package mybeans;
 
+import java.util.List;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import javax.persistence.Query;
-import java.util.List;
+import javax.persistence.NoResultException;
+import javax.persistence.NonUniqueResultException;
+import javax.persistence.TypedQuery;
+
 @Stateless
-public class UserDao {
+public class UserDao extends Dao<User> {
+
     @PersistenceContext(unitName = "unitDac")
     private EntityManager em;
-    public void save(User entity)
-    {
-        this.em.persist(entity);
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
     }
-    public void delete(User entity)
-    {
-        em.remove(em.merge(entity));
+
+    /**
+     * Get the User with the entity bean having the given primary key.
+     *
+     * @param id Primary key of the user.
+     * @return The user having id as a primary key.
+     */
+    public User getByIdUser(int id) {
+        TypedQuery<User> query = em.createNamedQuery("User.findByIdUser", User.class);
+        query = query.setParameter("idUser", id);
+        return query.getSingleResult();
     }
-    public void update(User entity)
-    {
-        em.merge(entity);
+
+    /**
+     * Get the User with the entity bean having the given mail field.
+     *
+     * Mail field has unique constraint in the DB, so there shouldn't be
+     * {@link NonUniqueResultException NonUniqueResultException} exception
+     * thrown.
+     *
+     * @param mail Mail of the user.
+     * @return The user having mail as email address.
+     */
+    public User getByMail(String mail) {
+        TypedQuery<User> query = em.createNamedQuery("User.findByMail", User.class);
+        query = query.setParameter("mail", mail);
+        return query.getSingleResult();
     }
-    public List<User> getAll()
-    {
-        Query query =em.createNamedQuery("User.findAll");
+
+    /**
+     * Get the list of all users.
+     * 
+     * @return The list of all users.
+     */
+    public List<User> getAll() {
+        TypedQuery<User> query = em.createNamedQuery("User.findAll", User.class);
         return query.getResultList();
-    }
-    public User getById(int id)
-    {
-        return (User)em.createNamedQuery("User.findByUserid").getSingleResult();
-    }
-    public User getByUserCode(String usercode)
-    {
-        Query query = em.createNamedQuery("User.findByUsercode");
-        query = query.setParameter("usercode",usercode);
-        return (User)query.getSingleResult();
-    }
-    public User getByUserName(String username)
-    {
-        Query query = em.createNamedQuery("User.findByUsername");
-        return (User)query.getSingleResult();
     }
 }
