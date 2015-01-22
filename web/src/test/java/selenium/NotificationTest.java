4774e1051359c05c8734907a7a57c53a0bd6b982
tonyguolei <tonyguolei@gmail.com>
2015-01-23 00:34:34 +0100

    add NotificationTest

---
+++ web/src/test/java/selenium/NotificationTest.java
@@ -0,0 +1,90 @@
+package selenium;
+
+import java.util.concurrent.TimeUnit;
+
+import junit.framework.TestCase;
+import org.junit.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class NotificationTest extends TestCase {
+    private WebDriver driver;
+    private String baseUrl;
+    private boolean acceptNextAlert = true;
+    private StringBuffer verificationErrors = new StringBuffer();
+
+    @Before
+    public void setUp() throws Exception {
+        driver = new FirefoxDriver();
+        baseUrl = "http://localhost:8080/";
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    }
+
+    @Test
+    public void testNotification() throws Exception {
+        driver.get(baseUrl + "/web/index.jsp");
+        driver.findElement(By.linkText("LOG IN")).click();
+        driver.findElement(By.name("mail")).clear();
+        driver.findElement(By.name("mail")).sendKeys("admin@dac.com");
+        driver.findElement(By.name("password")).clear();
+        driver.findElement(By.name("password")).sendKeys("dac");
+        driver.findElement(By.xpath("(//button[@type='submit'])[2]")).click();
+        driver.findElement(By.linkText("2")).click();
+        for (int second = 0;; second++) {
+            if (second >= 60) fail("timeout");
+            try { if ("It's a win, your project has been funded !".equals(driver.findElement(By.linkText("It's a win, your project has been funded !")).getText())) break; } catch (Exception e) {}
+            Thread.sleep(1000);
+        }
+
+        for (int second = 0;; second++) {
+            if (second >= 60) fail("timeout");
+            try { if ("It's a win, a project you have funded has reached its goal !".equals(driver.findElement(By.linkText("It's a win, a project you have funded has reached its goal !")).getText())) break; } catch (Exception e) {}
+            Thread.sleep(1000);
+        }
+
+        driver.findElement(By.linkText("admin@dac.com")).click();
+        driver.findElement(By.linkText("Logout")).click();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        driver.quit();
+        String verificationErrorString = verificationErrors.toString();
+        if (!"".equals(verificationErrorString)) {
+            fail(verificationErrorString);
+        }
+    }
+
+    private boolean isElementPresent(By by) {
+        try {
+            driver.findElement(by);
+            return true;
+        } catch (NoSuchElementException e) {
+            return false;
+        }
+    }
+
+    private boolean isAlertPresent() {
+        try {
+            driver.switchTo().alert();
+            return true;
+        } catch (NoAlertPresentException e) {
+            return false;
+        }
+    }
+
+    private String closeAlertAndGetItsText() {
+        try {
+            Alert alert = driver.switchTo().alert();
+            String alertText = alert.getText();
+            if (acceptNextAlert) {
+                alert.accept();
+            } else {
+                alert.dismiss();
+            }
+            return alertText;
+        } finally {
+            acceptNextAlert = true;
+        }
+    }
+}
