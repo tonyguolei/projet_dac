c4b1044a900a822e57ae764a9a58568e4fd448ee
tonyguolei <tonyguolei@gmail.com>
2015-01-23 16:56:53 +0100

    add SaveProjectTest

--- web/src/test/java/selenium/TestSuite.java
+++ web/src/test/java/selenium/TestSuite.java
@@ -8,7 +8,7 @@ public class TestSuite {
     public static Test suite() {
         junit.framework.TestSuite suite = new junit.framework.TestSuite();
 
-/*        suite.addTestSuite(CreateAccountTest.class);
+        suite.addTestSuite(CreateAccountTest.class);
         suite.addTestSuite(LoginTest.class);
         suite.addTestSuite(ModifyPasswordTest.class);
         suite.addTestSuite(CreateProjectTest.class);
@@ -16,10 +16,11 @@ public class TestSuite {
         suite.addTestSuite(ListProjectsTest.class);
         suite.addTestSuite(CreateBonusTest.class);
         suite.addTestSuite(CommentTest.class);
+        suite.addTestSuite(SaveProjectTest.class);
         suite.addTestSuite(FundProjectTest.class);
         suite.addTestSuite(ConsultBonusOwnerTest.class);
         suite.addTestSuite(AugmentContributionTest.class);
-        suite.addTestSuite(NotificationTest.class);*/
+        suite.addTestSuite(NotificationTest.class);
         suite.addTestSuite(ReportProjectTest.class);
 
         return suite;
