27f3012c4f0017bac83350aeb5920e5d1e05019b
tonyguolei <tonyguolei@gmail.com>
2015-01-23 16:30:55 +0100

    add ReportProjectTest

---
+++ web/src/test/java/selenium/ReportProjectTest.java
@@ -0,0 +1,93 @@
+package selenium;
+
+import java.util.concurrent.TimeUnit;
+import junit.framework.TestCase;
+import org.junit.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class ReportProjectTest extends TestCase{
+    private WebDriver driver;
+    private String baseUrl;
+    private boolean acceptNextAlert = true;
+    private StringBuffer verificationErrors = new StringBuffer();
+
+    @Before
+    public void setUp() throws Exception {
+        driver = new FirefoxDriver();
+        baseUrl = "http://localhost:8080/";
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    }
+
+    @Test
+    public void testReportProject() throws Exception {
+        driver.get(baseUrl + "/web/");
+        driver.findElement(By.linkText("LOG IN")).click();
+        driver.findElement(By.name("mail")).clear();
+        driver.findElement(By.name("mail")).sendKeys("testSelenium@dac.com");
+        driver.findElement(By.name("password")).clear();
+        driver.findElement(By.name("password")).sendKeys("test");
+        driver.findElement(By.xpath("(//button[@type='submit'])[2]")).click();
+        driver.findElement(By.linkText("Projects")).click();
+        driver.findElement(By.linkText("Browse")).click();
+        driver.findElement(By.linkText("testSelenium")).click();
+        driver.findElement(By.cssSelector("span.glyphicon.glyphicon-exclamation-sign")).click();
+        driver.findElement(By.cssSelector("div.modal-footer > button.btn.btn-primary")).click();
+        assertEquals("Project reported succefully!", driver.findElement(By.cssSelector("div.alert.alert-success")).getText());
+        driver.findElement(By.linkText("testSelenium@dac.com")).click();
+        driver.findElement(By.linkText("Logout")).click();
+        driver.findElement(By.linkText("LOG IN")).click();
+        driver.findElement(By.name("mail")).clear();
+        driver.findElement(By.name("mail")).sendKeys("admin@dac.com");
+        driver.findElement(By.name("password")).clear();
+        driver.findElement(By.name("password")).sendKeys("dac");
+        driver.findElement(By.xpath("(//button[@type='submit'])[2]")).click();
+        driver.findElement(By.linkText("Projects")).click();
+        driver.findElement(By.linkText("Browse")).click();
+        assertEquals("Yes", driver.findElement(By.cssSelector("font")).getText());
+        driver.findElement(By.linkText("admin@dac.com")).click();
+        driver.findElement(By.linkText("Logout")).click();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        driver.quit();
+        String verificationErrorString = verificationErrors.toString();
+        if (!"".equals(verificationErrorString)) {
+            fail(verificationErrorString);
+        }
+    }
+
+    private boolean isElementPresent(By by) {
+        try {
+            driver.findElement(by);
+            return true;
+        } catch (NoSuchElementException e) {
+            return false;
+        }
+    }
+
+    private boolean isAlertPresent() {
+        try {
+            driver.switchTo().alert();
+            return true;
+        } catch (NoAlertPresentException e) {
+            return false;
+        }
+    }
+
+    private String closeAlertAndGetItsText() {
+        try {
+            Alert alert = driver.switchTo().alert();
+            String alertText = alert.getText();
+            if (acceptNextAlert) {
+                alert.accept();
+            } else {
+                alert.dismiss();
+            }
+            return alertText;
+        } finally {
+            acceptNextAlert = true;
+        }
+    }
+}
--- web/src/test/java/selenium/TestSuite.java
+++ web/src/test/java/selenium/TestSuite.java
@@ -8,7 +8,7 @@ public class TestSuite {
     public static Test suite() {
         junit.framework.TestSuite suite = new junit.framework.TestSuite();
 
-        suite.addTestSuite(CreateAccountTest.class);
+/*        suite.addTestSuite(CreateAccountTest.class);
         suite.addTestSuite(LoginTest.class);
         suite.addTestSuite(ModifyPasswordTest.class);
         suite.addTestSuite(CreateProjectTest.class);
@@ -19,7 +19,8 @@ public class TestSuite {
         suite.addTestSuite(FundProjectTest.class);
         suite.addTestSuite(ConsultBonusOwnerTest.class);
         suite.addTestSuite(AugmentContributionTest.class);
-        suite.addTestSuite(NotificationTest.class);
+        suite.addTestSuite(NotificationTest.class);*/
+        suite.addTestSuite(ReportProjectTest.class);
 
         return suite;
     }
